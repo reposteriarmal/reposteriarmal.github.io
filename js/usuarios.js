b79efe7d09992c3ffcc7c8b91ab3695f977ff1ae
reposteriarmal <119449946+reposteriarmal@users.noreply.github.com>
2022-11-29 14:17:37 -0600

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -13,14 +13,14 @@ import {
 
 const SIN_ALUMNOS = /* html */
   `<option value="">
-    -- Sin Alumnos --
+    -- Sin Repostería --
   </option>`;
 
 const firestore = getFirestore();
 const daoRol = firestore.
   collection("Rol");
 const daoAlumno = firestore.
-  collection("Alumno");
+  collection("Reposteria");
 const daoUsuario = firestore.
   collection("Usuario");
 
