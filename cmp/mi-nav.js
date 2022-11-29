b464b050920321e20f0d6566163918467eb8f571
reposteriarmal <119449946+reposteriarmal@users.noreply.github.com>
2022-11-29 14:10:30 -0600

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -47,7 +47,7 @@ class MiNav extends HTMLElement {
         html += /* html */
           `<li>
             <a href=
-"alumnos.html">Alumnos</a>
+"alumnos.html">Repostería</a>
           </li>`;
       }
       this.ul.innerHTML += html;
